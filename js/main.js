516cffa7e75118d762c7ef130dd0cd3a7531549d
s98raghav@gmail.com <s98raghav@gmail.com>
2021-07-12 16:47:42 -0700

    Button Layout

--- js/main.js
+++ js/main.js
@@ -28,3 +28,38 @@ function operate(a, b, operation) {
         default: console.error("Invalid operator");
     }
 }
+
+function makeButtons() {
+    const panel = document.querySelector('.panel');
+    panel.style.gridTemplateColumns= "repeat("+3+", 1fr)" ;
+    panel.style.gridTemplateRows= "repeat("+4+", 1fr)";
+
+    for(let i = 1; i< 10; i++) {
+        const btn = document.createElement("BUTTON");
+        btn.innerText = i;
+        btn.classList.add('numberButton');
+        btn.id = i;
+        btn.style.fontSize='15px';
+        panel.appendChild(btn);
+    }
+    const z = document.createElement("BUTTON");
+    z.innerText = 0;
+    z.classList.add('numberButton');
+    z.id = 0;
+    z.style.fontSize='15px';
+    z.style.gridColumnStart = '1';
+    z.style.gridColumnEnd = 'span 2';
+    panel.appendChild(z);
+
+
+    const e = document.createElement("BUTTON");
+    e.innerText = '=';
+    e.classList.add('operator');
+    e.id = 'equals';
+    e.style.fontSize='15px';
+    panel.appendChild(e);
+}
+
+window.onload = function() {
+    makeButtons();
+};
